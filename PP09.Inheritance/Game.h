ef08de528b2e59740611fc4231cd39fd53acc47d
20142367 <20142367@vision.hoseo.edu>
2018-10-18 17:31:55 +0900

    10월 18일 PP09.Inheritance

---
+++ PP09.Inheritance/Game.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include "Player.h"
+
+using namespace std;
+
+class Game
+{
+public:
+	Game() {}
+	~Game() {}
+
+	GameObject m_go;
+	Player m_player;
+
+	bool init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+
+	void render();
+	void update();
+	void handleEvents();
+	void clean();
+	bool running() { return m_bRunning; }
+
+private:
+	SDL_Window * m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	SDL_RendererFlip* m_pRendererFlip;
+
+	int m_currentFrame;
+	TextureManager* m_textureManager;
+
+	bool m_bRunning;
+};
