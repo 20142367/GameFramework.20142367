1f14347152073795388f9abd3a8eafc3ba4e708a
20142367 <20142367@vision.hoseo.edu>
2018-10-25 18:15:28 +0900

    181025 임시 저장

---
+++ PP11.AbstractClass/Enemy.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "SDLGameObject.h"
+
+class Enemy : public SDLGameObject
+{
+public:
+	/*
+	void update();
+	void clean() {};
+	*/
+
+	Enemy(const LoaderParams* pParams);
+
+	virtual void draw();
+	virtual void update();
+	virtual void clean();
+};
