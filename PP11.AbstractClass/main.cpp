375d86989373ba901e528e39acbb8a536ddc421a
무명 무명 <alsrl0033@gmail.com>
2018-10-28 23:51:47 +0900

    10월 28일 PP12.Vector2D

--- PP11.AbstractClass/main.cpp
+++ PP11.AbstractClass/main.cpp
@@ -2,7 +2,9 @@
 
 // Game* g_game = 0;	// our Game object
 // Game* Game::s_pInstance = 0;
-
+const int FPS = 60;
+const float DELAY_TIME = 1000.0f / FPS;
+Uint32 frameStart, frameTime;
 
 int main(int argc, char* argv[]) {
 	/*
@@ -23,15 +25,30 @@ int main(int argc, char* argv[]) {
 	*/
 
 	std::cout << "game init attempt...\n";
-	if (TheGame::Instance()->init("PP11.AbstractClass", 100, 100, 640, 480, false))
+	if (TheGame::Instance()->init("PP12.Vector2D", 100, 100, 640, 480, false))
 	{
 		std::cout << "game init success!\n";
+		/*
 		while (TheGame::Instance()->running())
 		{
 			TheGame::Instance()->handleEvents();
 			TheGame::Instance()->update();
 			TheGame::Instance()->render();
 			SDL_Delay(10);
+		}
+		*/
+		while (TheGame::Instance()->running())
+		{
+			frameStart = SDL_GetTicks();
+			TheGame::Instance()->handleEvents();
+			TheGame::Instance()->update();
+			TheGame::Instance()->render();
+			frameTime = SDL_GetTicks() - frameStart;
+
+			if (frameTime < DELAY_TIME)
+			{
+				SDL_Delay((int)(DELAY_TIME - frameTime));
+			}
 		}
 	}
 	else {
